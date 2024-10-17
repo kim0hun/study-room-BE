28a76062584dab045e3f7760753293741a7c8a6b
Kim0hun <dudgns9194@gmail.com>
2024-10-18 00:25:21 +0900

    [FIX] 플래너 수정 (#45)
    * fix: 방 나가기 시 방장 정보 수정
    
    * fix: RoomAndMyInfoDto 수정
    
    * fix: 플래너 생성 버그 수정
    
    * fix: 플래너 수정 버그 수정

--- src/planners/planners.service.ts
+++ src/planners/planners.service.ts
@@ -41,6 +41,7 @@ export class PlannersService {
         // 시간이 겹치는 날짜 탐색
         const overlappingPlanners = await this.plannerModel.find({
           date: plannerDto.date,
+          userId: new Types.ObjectId(userId),
           $or: [
             {
               startTime: { $lt: plannerDto.endTime },
@@ -250,6 +251,7 @@ export class PlannersService {
       // 시간이 겹치는 할 일 탐색
       const overlappingPlanners = await this.plannerModel.find({
         date: plannerDto.date,
+        userId: new Types.ObjectId(userId),
         $or: [
           {
             startTime: { $lt: plannerDto.endTime },
